b0c51ee0f073becaba6ca36f8655d8736e37d242
Zachran Razendra <zachranraze@recodex.id>
2024-12-31 12:31:12 +0700

    Add Laravel Debugbar package for debugging
    Integrated Laravel Debugbar by updating `composer.json`, adding configuration files, and registering necessary service providers and aliases. This enhances debugging capabilities during development by providing detailed insights into application performance and behavior.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Barryvdh\Debugbar\Facades\Debugbar;
+use Illuminate\Foundation\AliasLoader;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +13,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $loader = AliasLoader::getInstance();
+        $loader->alias('Debugbar', Debugbar::class);
     }
 
     /**
