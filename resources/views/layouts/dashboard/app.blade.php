aecfbb7043f0d04e6d8f6acddc7a49631afdeebd
Zachran Razendra <zachranraze@recodex.id>
2025-01-02 05:02:43 +0700

    Add Flaticon support and define icons configuration
    Introduce a new `icons.php` config file specifying available icons. Update the dashboard layout to include the Flaticon stylesheet for consistent icon usage.

--- resources/views/layouts/dashboard/app.blade.php
+++ resources/views/layouts/dashboard/app.blade.php
@@ -11,6 +11,9 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet"/>
 
+    <!--====== Flaticon css ======-->
+    <link rel="stylesheet" href="{{ asset('main/fonts/flaticon/flaticon_gowilds.css') }}">
+
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
