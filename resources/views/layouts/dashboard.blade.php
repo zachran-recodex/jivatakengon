8ebe0899735c5ff79b0322291a89ea917ce3d96a
Zachran Razendra <zachranraze@recodex.id>
2025-02-15 16:34:54 +0700

    Add dashboard and main layout templates with responsive design and navigation

---
+++ resources/views/layouts/dashboard.blade.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+        <!-- Icons -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+        <!-- Styles -->
+        @livewireStyles
+    </head>
+    <body class="font-sans antialiased bg-background-light">
+        <div x-data="{ sidebarOpen: false }" class="min-h-screen">
+            <!-- Off-canvas menu for mobile -->
+            <div x-show="sidebarOpen" class="relative z-50 lg:hidden" x-description="Off-canvas menu for mobile"
+                 role="dialog" aria-modal="true">
+                <div x-show="sidebarOpen" x-transition:enter="transition-opacity ease-linear duration-300"
+                     x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100"
+                     x-transition:leave="transition-opacity ease-linear duration-300" x-transition:leave-start="opacity-100"
+                     x-transition:leave-end="opacity-0" class="fixed inset-0 bg-shark-950/80"></div>
+
+                <div class="fixed inset-0 flex">
+                    <div x-show="sidebarOpen" x-transition:enter="transition ease-in-out duration-300 transform"
+                         x-transition:enter-start="-translate-x-full" x-transition:enter-end="translate-x-0"
+                         x-transition:leave="transition ease-in-out duration-300 transform"
+                         x-transition:leave-start="translate-x-0" x-transition:leave-end="-translate-x-full"
+                         class="relative flex flex-1 w-full max-w-xs mr-16">
+                        <div class="absolute top-0 flex justify-center w-16 pt-5 left-full">
+                            <button type="button" class="-m-2.5 p-2.5" @click="sidebarOpen = false">
+                                <span class="sr-only">Close sidebar</span>
+                                <i class="fa-regular fa-circle-xmark w-6 h-6 text-shark-50"></i>
+                            </button>
+                        </div>
+
+                        <!-- Sidebar component for mobile -->
+                        <div
+                            class="flex flex-col px-6 pb-4 overflow-y-auto bg-white border-r border-shark-200 grow gap-y-5">
+                            <div class="flex items-center h-16 shrink-0 border-b border-shark-200">
+                                <h3 class="text-2xl font-bold tracking-tight text-primary">
+                                    JIVA TAKENGON
+                                </h3>
+                            </div>
+                            @include('layouts.partials.navigation-menu')
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Static sidebar for desktop -->
+            <div class="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-64 lg:flex-col">
+                <div class="flex flex-col px-6 pb-4 overflow-y-auto bg-white border-r border-shark-200 grow gap-y-5">
+                    <div class="flex items-center h-16 shrink-0 border-b border-shark-200">
+                        <a href="" class="text-2xl font-bold tracking-tight text-primary">
+                            JIVA TAKENGON
+                        </a>
+                    </div>
+                    @include('layouts.partials.navigation-menu')
+                </div>
+            </div>
+
+            <div class="lg:pl-64">
+                <!-- Top header -->
+                @include('layouts.partials.header')
+
+                <!-- Main content -->
+                <main class="py-10">
+                    <div class="px-4 sm:px-6 lg:px-8">
+                        {{ $slot }}
+                    </div>
+                </main>
+            </div>
+        </div>
+
+        @stack('modals')
+
+        @livewireScripts
+    </body>
+</html>
