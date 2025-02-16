37a838e69a0d14786003c7ab804ab58b9a9a8113
Zachran Razendra <zachranraze@recodex.id>
2025-02-17 06:54:42 +0700

    Refactor profile view to use dashboard layout for improved consistency and structure

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -1,10 +1,4 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Profile') }}
-        </h2>
-    </x-slot>
-
+<x-dashboard-layout>
     <div>
         <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
             @if (Laravel\Fortify\Features::canUpdateProfileInformation())
@@ -42,4 +36,4 @@
             @endif
         </div>
     </div>
-</x-app-layout>
+</x-dashboard-layout>
